c30fcee20662c56611b75d4d7c112b29f337faa9
Xie Yuheng <xyheme@gmail.com>
2022-08-27 23:56:59 +0800

    more tests for check-pi

--- src/lang/tests/check-pi.test.ts
+++ src/lang/tests/check-pi.test.ts
@@ -19,11 +19,28 @@ check (A: Type, B: Type) -> Type: Type
 `)
 })
 
+test("check Pi is a Type -- telescope", async () => {
+  const mod = new Mod()
+  await mod.run(`
+
+check (A: Type, B: Type, x: A) -> B: Type
+
+`)
+})
+
 test("check Pi is a Type -- nested", async () => {
   const mod = new Mod()
   await mod.run(`
 
-check (A: Type, F: (A: Type) -> Type) -> Type: Type
+check (
+  A: Type,
+  F: (A: Type) -> Type,
+) -> Type: Type
+
+check (
+  F: (A: Type) -> Type,
+  G: (A: Type, B: Type) -> Type,
+) -> Type: Type
 
 `)
 })
