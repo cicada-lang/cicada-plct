3da731ccc4e23363a26176737637619606c96f2f
Xie Yuheng <xyheme@gmail.com>
2022-08-28 16:19:36 +0800

    add parse Var with span

--- src/lang/parse/tests/exps/var.test.ts
+++ src/lang/parse/tests/exps/var.test.ts
@@ -7,3 +7,9 @@ test("parse Var", () => {
   expect(parseExp("x")).toMatchObject(deleteUndefined(Var("x")))
   expect(parseExp("abc")).toMatchObject(deleteUndefined(Var("abc")))
 })
+
+test("parse Var -- with span", () => {
+  expect(parseExp("x")).toMatchObject(Var("x", { lo: 0, hi: 1 }))
+  expect(parseExp("abc")).toMatchObject(Var("abc", { lo: 0, hi: 3 }))
+  expect(parseExp("  abc")).toMatchObject(Var("abc", { lo: 2, hi: 5 }))
+})
