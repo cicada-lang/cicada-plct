29814dd1d74d02bc562fadf469186b7f1bd22f58
简 <2447746158@qq.com>
2022-08-28 14:50:18 +0800

    add check for sigma and test

---
+++ src/lang/tests/check-sigma.test.ts
@@ -0,0 +1,10 @@
+import { test } from "vitest"
+import { expectCodeToRun } from "./utils"
+
+test("check Sigma is a Type", async () => {
+  await expectCodeToRun(`
+
+check exists (n : Type) Type : Type
+
+`)
+})
