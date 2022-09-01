866379aa23256cd747583e9c9a3da9f820638200
简 <2447746158@qq.com>
2022-09-01 09:58:26 +0800

    add compute-sigma.test.ts

---
+++ src/lang/tests/compute-sigma.test.ts
@@ -0,0 +1,12 @@
+import { expect, test } from "vitest"
+import { runCode } from "./utils"
+
+test("compute sigma", async () => {
+  const output = await runCode(`
+
+compute exists (Type) Type
+
+`)
+
+  expect(output).toMatchInlineSnapshot('"exists (Type) Type: Type"')
+})
