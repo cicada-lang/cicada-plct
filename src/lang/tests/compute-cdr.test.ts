3e67b31fba742ec74f70ffde98a46f9ae63b032f
简 <2447746158@qq.com>
2022-09-01 10:06:28 +0800

    add compute-car.test.ts & compute-cdr.test.ts

---
+++ src/lang/tests/compute-cdr.test.ts
@@ -0,0 +1,13 @@
+import { expect, test } from "vitest"
+import { runCode } from "./utils"
+
+test("compute cdr", async () => {
+  const output = await runCode(`
+
+let pair: exists (Type) Type = cons(Type, Type)
+compute cdr(pair)
+
+`)
+
+  expect(output).toMatchInlineSnapshot('"Type: Type"')
+})
