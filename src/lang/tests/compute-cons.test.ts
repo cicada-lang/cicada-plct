6fdeb01a558bbcd55f6d1eac42c73b02fd496658
简 <2447746158@qq.com>
2022-09-01 10:04:45 +0800

    add compute-cons.test.ts

---
+++ src/lang/tests/compute-cons.test.ts
@@ -0,0 +1,13 @@
+import { expect, test } from "vitest"
+import { runCode } from "./utils"
+
+test("compute cons", async () => {
+  const output = await runCode(`
+
+let pair: exists (Type) Type = cons(Type, Type)
+compute pair
+
+`)
+
+  expect(output).toMatchInlineSnapshot('"cons(Type, Type): exists (Type) Type"')
+})
