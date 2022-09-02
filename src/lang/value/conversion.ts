e83fa76b67815ad2703391ec92abcdab79ff5da2
简 <2447746158@qq.com>
2022-09-03 00:17:32 +0800

    add conversion placeholder

---
+++ src/lang/value/conversion.ts
@@ -0,0 +1,11 @@
+import { Ctx } from "../ctx"
+import { Value } from "./Value"
+
+export function conversion(
+  ctx: Ctx,
+  type: Value,
+  left: Value,
+  right: Value
+): void {
+  throw "TODO"
+}
