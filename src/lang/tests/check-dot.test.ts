e9f07003ae023a85190af02d80d27438c1142445
Xie Yuheng <xyheme@gmail.com>
2022-09-03 18:57:43 +0800

    check dot -- abc test

--- src/lang/tests/check-dot.test.ts
+++ src/lang/tests/check-dot.test.ts
@@ -4,7 +4,10 @@ import { runCode } from "./utils"
 test("check Dot", async () => {
   await runCode(`
 
-let obj: class { a: String, b: String } = { a: "a", b: "b" }
-check obj.a: String
+let abc: class { a: String, b: String, c: String } = { a: "a", b: "b", c: "c" }
+
+check abc.a: String
+check abc.b: String
+check abc.c: String
   `)
 })
