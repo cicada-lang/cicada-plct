d887a26c911e9ae76a6ece0246278dc2d7b6e95d
简 <2447746158@qq.com>
2022-09-03 20:23:28 +0800

    [refactor] rename readbackObjekt to readbackProperties

--- src/lang/value/readbackProperties.ts
+++ src/lang/value/readbackProperties.ts
@@ -4,41 +4,35 @@ import { Ctx } from "../ctx"
 import * as Values from "../value"
 import { assertClazzInCtx, readback, Value } from "../value"
 
-export function readbackObjekt(
+export function readbackProperties(
   ctx: Ctx,
   type: Values.Clazz,
   value: Value
-): Cores.Objekt {
+): Record<string, Cores.Core> {
   switch (type.kind) {
     case "ClazzNull": {
-      return Cores.Objekt({})
+      return {}
     }
 
     case "ClazzCons": {
       const propertyValue = Actions.doDot(value, type.name)
       const rest = Values.applyClosure(type.restClosure, propertyValue)
       assertClazzInCtx(ctx, rest)
-
       const propertyCore = readback(ctx, type.propertyType, propertyValue)
-      const restCore = readbackObjekt(ctx, rest, value)
-
-      return Cores.Objekt({
+      return {
         [type.name]: propertyCore,
-        ...restCore.properties,
-      })
+        ...readbackProperties(ctx, rest, value),
+      }
     }
 
     case "ClazzFulfilled": {
       const propertyValue = Actions.doDot(value, type.name)
       const rest = type.rest
-
       const propertyCore = readback(ctx, type.propertyType, propertyValue)
-      const restCore = readbackObjekt(ctx, rest, value)
-
-      return Cores.Objekt({
+      return {
         [type.name]: propertyCore,
-        ...restCore.properties,
-      })
+        ...readbackProperties(ctx, rest, value),
+      }
     }
   }
 }
