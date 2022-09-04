6226e54fa4d80df5c0cca194d241bf3f20db75b7
简 <2447746158@qq.com>
2022-09-05 06:52:39 +0800

    remove Cores.New

--- src/lang/core/Core.ts
+++ src/lang/core/Core.ts
@@ -10,7 +10,6 @@ export type Core =
   | Quote
   | Clazz
   | Objekt
-  | New
   | Dot
   | Let
   | LetThe
@@ -239,22 +238,6 @@ export function Objekt(properties: Record<string, Core>): Objekt {
   return {
     family: "Core",
     kind: "Objekt",
-    properties,
-  }
-}
-
-export type New = {
-  family: "Core"
-  kind: "New"
-  name: string
-  properties: Record<string, Core>
-}
-
-export function New(name: string, properties: Record<string, Core>): New {
-  return {
-    family: "Core",
-    kind: "New",
-    name,
     properties,
   }
 }
