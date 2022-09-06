a92eea609e4caa827f7d5dfc13f4ea7a5662886a
Xie Yuheng <xyheme@gmail.com>
2022-09-07 05:46:29 +0800

    add computation to conversion tests

---
+++ src/lang/tests/conversion/conversion-clazz.test.ts
@@ -0,0 +1,29 @@
+import { test } from "vitest"
+import { expectCodeToFail, runCode } from "../utils"
+
+test("conversion Clazz", async () => {
+  await runCode(`
+
+conversion Type {
+  // class { A: Type, x: A }
+  // class { A: Type, x: A }
+}
+
+conversion Type {
+  // class { A: Type, B: Type, pair: Pair(A, B) }
+  // class { B: Type, B: Type, pair: Pair(A, B) }
+}
+
+`)
+})
+
+test.todo("conversion Clazz -- fail", async () => {
+  await expectCodeToFail(`
+
+conversion Type {
+  // class { A: Type, x: A }
+  // class { A: Type, x: A }
+}
+
+`)
+})
--- src/lang/tests/conversion/conversion-sigma.test.ts
+++ src/lang/tests/conversion/conversion-sigma.test.ts
@@ -4,14 +4,21 @@ import { expectCodeToFail, runCode } from "../utils"
 test("conversion Sigma", async () => {
   await runCode(`
 
+function id(T: Type, x: T): T {
+  return x
+}
+
 conversion Type {
   exists (A: Type) A
   exists (B: Type) B
+  exists (B: Type) id(Type, B)
 }
 
 conversion Type {
   exists (A: Type, B: Type) Pair(A, B)
   exists (B: Type, A: Type) Pair(B, A)
+  exists (B: Type, A: Type) Pair(id(Type, B), id(Type, A))
+  exists (B: Type, A: id(Type, Type)) Pair(id(Type, B), id(Type, A))
 }
 
 `)
