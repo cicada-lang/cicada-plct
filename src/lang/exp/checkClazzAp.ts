91b5614fdc7cef78c093b820ce1e9f45902c1eed
Xie Yuheng <xyheme@gmail.com>
2022-09-09 06:46:00 +0800

    fix typing of `checkClazzAp`

--- src/lang/exp/checkClazzAp.ts
+++ src/lang/exp/checkClazzAp.ts
@@ -1,11 +1,11 @@
 import { Core } from "../core"
 import { Ctx } from "../ctx"
 import { ElaborationError } from "../errors"
-import { Value } from "../value"
+import * as Values from "../value"
 import { check } from "./check"
 import { Exp } from "./Exp"
 
-export function checkClazzAp(ctx: Ctx, clazz: Value, arg: Exp): Core {
+export function checkClazzAp(ctx: Ctx, clazz: Values.Clazz, arg: Exp): Core {
   switch (clazz.kind) {
     case "ClazzNull": {
       throw new ElaborationError("cannot apply argument to ClazzNull")
@@ -17,12 +17,6 @@ export function checkClazzAp(ctx: Ctx, clazz: Value, arg: Exp): Core {
 
     case "ClazzFulfilled": {
       return checkClazzAp(ctx, clazz.rest, arg)
-    }
-
-    default: {
-      throw new ElaborationError(
-        `checkClazzAp expected a Clazz, but got ${clazz.kind}`,
-      )
     }
   }
 }
