c80719ec2d9ecb20623c2dbb147e14f3fc95af7f
jwyjohn <1951510@tongji.edu.cn>
2022-09-13 14:59:00 +0800

    trying to implement *infer Cons to Pair*

--- src/lang/tests/check/check-cons.test.ts
+++ src/lang/tests/check/check-cons.test.ts
@@ -1,4 +1,4 @@
-import { test } from "vitest"
+import { expect, test } from "vitest"
 import { runCode } from "../utils"
 
 test("check Cons", async () => {
@@ -7,4 +7,19 @@ test("check Cons", async () => {
 check cons(Type, Type): exists (Type) Type
 
 `)
+})
+
+test("infer Cons to Pair if both car and cdr can be inferred", async () => {
+  const output = await runCode(`
+
+let pair = cons(cons(cons(Type, "Str1"), cons(sole, "Str1")), Type)
+compute pair
+
+`)
+
+  expect(output).toMatchInlineSnapshot(
+    '"cons(cons(cons(Type, \\"Str1\\"), cons(sole, \\"Str1\\")), Type): exists (exists (exists (Type) String, Trivial) String) Type"',
+  )
+
+  // throw new Error(output)
 })
