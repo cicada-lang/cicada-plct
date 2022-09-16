a53a427d1aedf4a65394c0f98c583b62917bf904
简 <2447746158@qq.com>
2022-09-16 13:40:10 +0800

    add basic test for FnWithRetType

---
+++ src/lang/tests/compute/compute-fn-with-return-type.test.ts
@@ -0,0 +1,17 @@
+import { expect, test } from "vitest"
+import { runCode } from "../utils"
+
+test.todo("compute Fn with return type", async () => {
+  const output = await runCode(`
+
+let id = function (T: Type, x: T): T x
+compute id
+compute id(Type)
+
+`)
+
+  expect(output).toMatchInlineSnapshot(`
+    "(T, x) => x: (T: Type, x: T) -> T
+    (x) => x: (x: Type) -> Type"
+  `)
+})
