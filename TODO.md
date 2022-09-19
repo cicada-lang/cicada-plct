675633e89da0b6247db4eadb4337692affa37ea2
Xie Yuheng <xyheme@gmail.com>
2022-09-20 07:44:45 +0800

    extract top level `insertion/` dir

--- TODO.md
+++ TODO.md
@@ -1,3 +1,9 @@
 # implicit-ap-insertion-during-check
 
+extract insertion/solveArgTypes
+extract insertion/applyInsertions
+extract insertion/solveRetType
+
+inline insertImplicitAp
+
 checkByInfer -- when inferred type is ImplicitPi, handle it specially
---
+++ src/lang/insertion/Insertion.ts
@@ -0,0 +1,172 @@
+import { applyClosure } from "../closure"
+import * as Cores from "../core"
+import { Core, evaluate } from "../core"
+import { Ctx, CtxCons, ctxNames, ctxToEnv } from "../ctx"
+import { ElaborationError } from "../errors"
+import * as Exps from "../exp"
+import { check } from "../exp"
+import {
+  createPatternVar,
+  deepWalk,
+  lookupValueInSolution,
+  PatternVar,
+  Solution,
+  solutionNames,
+  solveType,
+} from "../solution"
+import { freshen } from "../utils/freshen"
+import { readback, Value } from "../value"
+
+export function solveArgTypes(
+  solution: Solution,
+  ctx: Ctx,
+  type: Value,
+  args: Array<Exps.Arg>,
+  insertions: Array<Insertion> = [],
+): {
+  solution: Solution
+  type: Value
+  insertions: Array<Insertion>
+} {
+  const [arg, ...restArgs] = args
+
+  if (arg === undefined) {
+    return { solution, type, insertions }
+  }
+
+  if (type.kind === "ImplicitPi" && arg.kind === "ArgPlain") {
+    const name = type.retTypeClosure.name
+    // TODO Scope BUG, `freshName` might occurs in `args`.
+    const usedNames = [...ctxNames(ctx), ...solutionNames(solution)]
+    const freshName = freshen(usedNames, name)
+    const patternVar = createPatternVar(type.argType, freshName)
+    return solveArgTypes(
+      solution,
+      // TODO Why we need to extend `ctx` here?
+      CtxCons(freshName, type.argType, ctx),
+      applyClosure(type.retTypeClosure, patternVar),
+      args, // NOTE Do not consume arg here.
+      [...insertions, InsertionPatternVar(patternVar)],
+    )
+  }
+
+  if (type.kind === "Pi" && arg.kind === "ArgPlain") {
+    const argInferred = Exps.inferOrUndefined(ctx, arg.exp)
+    if (argInferred !== undefined) {
+      solution = solveType(solution, ctx, argInferred.type, type.argType)
+    }
+
+    // TODO Do we need to call `deepWalk` here?
+    const argCore = argInferred
+      ? argInferred.core
+      : check(ctx, arg.exp, type.argType)
+    const argValue = evaluate(ctxToEnv(ctx), argCore)
+    return solveArgTypes(
+      solution,
+      ctx,
+      applyClosure(type.retTypeClosure, argValue),
+      restArgs,
+      [...insertions, InsertionUsedArg(argCore)],
+    )
+  }
+
+  if (type.kind === "ImplicitPi" && arg.kind === "ArgImplicit") {
+    const argCore = Exps.check(ctx, arg.exp, type.argType)
+    const argValue = evaluate(ctxToEnv(ctx), argCore)
+    return solveArgTypes(
+      solution,
+      ctx,
+      applyClosure(type.retTypeClosure, argValue),
+      restArgs,
+      [...insertions, InsertionImplicitArg(argCore)],
+    )
+  }
+
+  if (type.kind === "Pi" && arg.kind === "ArgImplicit") {
+    throw new ElaborationError(`extra Implicit argument`)
+  }
+
+  throw new ElaborationError(
+    `expect type to be Pi or ImplicitPi instead of: ${type.kind}`,
+  )
+}
+
+export type Insertion =
+  | InsertionPatternVar
+  | InsertionUsedArg
+  | InsertionImplicitArg
+
+export type InsertionPatternVar = {
+  kind: "InsertionPatternVar"
+  patternVar: PatternVar
+}
+
+export function InsertionPatternVar(
+  patternVar: PatternVar,
+): InsertionPatternVar {
+  return {
+    kind: "InsertionPatternVar",
+    patternVar,
+  }
+}
+
+export type InsertionUsedArg = {
+  kind: "InsertionUsedArg"
+  argCore: Core
+}
+
+export function InsertionUsedArg(argCore: Core): InsertionUsedArg {
+  return {
+    kind: "InsertionUsedArg",
+    argCore,
+  }
+}
+
+export type InsertionImplicitArg = {
+  kind: "InsertionImplicitArg"
+  argCore: Core
+}
+
+export function InsertionImplicitArg(argCore: Core): InsertionImplicitArg {
+  return {
+    kind: "InsertionImplicitArg",
+    argCore,
+  }
+}
+
+export function applyInsertion(
+  solution: Solution,
+  ctx: Ctx,
+  insertion: Insertion,
+  core: Core,
+): Core {
+  switch (insertion.kind) {
+    case "InsertionPatternVar": {
+      let argValue = lookupValueInSolution(
+        solution,
+        insertion.patternVar.neutral.name,
+      )
+      if (argValue === undefined) {
+        throw new ElaborationError(
+          `Unsolved patternVar: ${insertion.patternVar.neutral.name}`,
+        )
+      }
+
+      const argCore = readback(
+        ctx,
+        deepWalk(solution, ctx, insertion.patternVar.type),
+        deepWalk(solution, ctx, argValue),
+      )
+
+      return Cores.ImplicitAp(core, argCore)
+    }
+
+    case "InsertionUsedArg": {
+      return Cores.Ap(core, insertion.argCore)
+    }
+
+    case "InsertionImplicitArg": {
+      return Cores.ImplicitAp(core, insertion.argCore)
+    }
+  }
+}
