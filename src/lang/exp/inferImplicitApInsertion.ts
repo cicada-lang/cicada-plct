193682d0b35a54675d91d005cc71c2a872ff9327
Xie Yuheng <xyheme@gmail.com>
2022-09-20 07:24:15 +0800

    remove comment -- express idea by code

--- src/lang/exp/inferImplicitApInsertion.ts
+++ src/lang/exp/inferImplicitApInsertion.ts
@@ -7,21 +7,13 @@ export function inferImplicitApInsertion(
   ctx: Ctx,
   exp: Exp,
 ): Inferred | undefined {
-  /**
-     Maybe `ImplicitAp` insertion.
-  **/
   switch (exp.kind) {
     case "Ap": {
       const { target, args } = Exps.foldAp(exp)
-      const inferred = infer(ctx, target)
-      if (inferred.type.kind === "ImplicitPi" && args[0]?.kind === "ArgPlain") {
-        return Exps.insertImplicitAp(
-          SolutionNull(),
-          ctx,
-          inferred.type,
-          inferred.core,
-          args,
-        )
+      const { type, core } = infer(ctx, target)
+      if (type.kind === "ImplicitPi") {
+        const solution = SolutionNull()
+        return Exps.insertImplicitAp(solution, ctx, type, core, args)
       }
     }
   }
