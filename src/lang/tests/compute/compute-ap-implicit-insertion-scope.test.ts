36dc7b9c5a3d47c9c06784e342c3b7ecf2d18bee
Xie Yuheng <xyheme@gmail.com>
2022-09-21 21:33:17 +0800

    update test "compute ApImplicit -- scope"

--- src/lang/tests/compute/compute-ap-implicit-insertion-scope.test.ts
+++ src/lang/tests/compute/compute-ap-implicit-insertion-scope.test.ts
@@ -2,28 +2,32 @@ import { expect, test } from "vitest"
 import { runCode } from "../utils"
 
 /**
-   TODO We need to design a failing test, before we try to fix `inferApPiImplicit`.
+
+   TODO We need to design a failing test,
+   before we try to fix `inferApPiImplicit`.
+
 **/
 
-test("compute ApImplicit -- Scope", async () => {
+/**
+
+   TODO We need to test throw `LangError`,
+   intead of using `expectCodeToFail` which assert all errors.
+
+**/
+
+test.todo("compute ApImplicit -- scope", async () => {
   const output = await runCode(`
 
-let A: Type = String
-let A1: Trivial = sole
-
-function twins(implicit A: Type, A1: A): Pair(A, A) {
-  return cons(A1, A1)
+function id(implicit A: Type, x: A): A {
+  return x
 }
 
-compute twins("1")
-compute twins(A)
-compute twins(A1)
+compute id((x: A) => x)
+// compute id((x: A1) => x)
+// compute id(id((x: A1) => x))
+// compute id(id(id((x: A1) => x)))
 
 `)
 
-  expect(output).toMatchInlineSnapshot(`
-    "cons(\\"1\\", \\"1\\"): exists (String) String
-    cons(String, String): exists (Type) Type
-    cons(sole, sole): exists (Trivial) Trivial"
-  `)
+  expect(output).toMatchInlineSnapshot()
 })
