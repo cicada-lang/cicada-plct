aaf185acd431c7354a367065f70a72bd8b92195a
Xie Yuheng <xyheme@gmail.com>
2022-09-21 20:43:05 +0800

    try to add compute-ap-implicit-insertion-scope.test.ts

---
+++ src/lang/tests/compute/compute-ap-implicit-insertion-scope.test.ts
@@ -0,0 +1,25 @@
+import { expect, test } from "vitest"
+import { runCode } from "../utils"
+
+test("compute ApImplicit -- Scope", async () => {
+  const output = await runCode(`
+
+let A: Type = String
+let A1: Trivial = sole
+
+function twins(implicit A: Type, A1: A): Pair(A, A) {
+  return cons(A1, A1)
+}
+
+compute twins("1")
+compute twins(A)
+compute twins(A1)
+
+`)
+
+  expect(output).toMatchInlineSnapshot(`
+    "cons(\\"1\\", \\"1\\"): exists (String) String
+    cons(String, String): exists (Type) Type
+    cons(sole, sole): exists (Trivial) Trivial"
+  `)
+})
