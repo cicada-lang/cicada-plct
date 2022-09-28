09239d1dea425a7642a2f4e7acb9740fc5c0d002
Xie Yuheng <xyheme@gmail.com>
2022-09-28 18:03:02 +0800

    pass test "compute ApImplicit -- scope"

--- TODO.md
+++ TODO.md
@@ -1,8 +1,10 @@
 [bug] implement freeNames to fix the scope BUG of ImplicitAp insertion
 
-- https://github.com/cicada-lang/cicada/issues/72
+- finish `exp/freeNames`
 
-- `exp/freeNames`
+- test "compute ApImplicit -- scope" -- should `LangError` -- not just `expectCodeToFail`
+
+- https://github.com/cicada-lang/cicada/issues/72
 
 [feature] deepWalk
 
--- src/lang/exp/freeNames.ts
+++ src/lang/exp/freeNames.ts
@@ -1,3 +1,4 @@
+import * as Exps from "../exp"
 import { Exp } from "../exp"
 
 export function freeNames(boundNames: Set<string>, exp: Exp): Set<string> {
@@ -6,7 +7,174 @@ export function freeNames(boundNames: Set<string>, exp: Exp): Set<string> {
       return boundNames.has(exp.name) ? new Set() : new Set([exp.name])
     }
 
-    default: {
+    case "Pi": {
+      return new Set([
+        ...freeNames(boundNames, exp.argType),
+        ...freeNames(new Set([...boundNames, exp.name]), exp.retType),
+      ])
+    }
+
+    case "PiImplicit": {
+      return new Set([
+        ...freeNames(boundNames, exp.argType),
+        ...freeNames(new Set([...boundNames, exp.name]), exp.retType),
+      ])
+    }
+
+    case "PiUnfolded": {
+      return freeNames(boundNames, Exps.foldPi(exp.bindings, exp.retType))
+    }
+
+    case "Ap": {
+      return new Set([
+        ...freeNames(boundNames, exp.target),
+        ...freeNames(boundNames, exp.arg),
+      ])
+    }
+
+    case "ApImplicit": {
+      return new Set([
+        ...freeNames(boundNames, exp.target),
+        ...freeNames(boundNames, exp.arg),
+      ])
+    }
+
+    case "ApUnfolded": {
+      return freeNames(boundNames, Exps.foldAp(exp.target, exp.args))
+    }
+
+    case "Fn": {
+      return new Set([
+        ...freeNames(new Set([...boundNames, exp.name]), exp.ret),
+      ])
+    }
+
+    case "FnAnnotated": {
+      return new Set([
+        ...freeNames(new Set(boundNames), exp.argType),
+        ...freeNames(new Set([...boundNames, exp.name]), exp.ret),
+      ])
+    }
+
+    case "FnImplicit": {
+      return new Set([
+        ...freeNames(new Set([...boundNames, exp.name]), exp.ret),
+      ])
+    }
+
+    case "FnImplicitAnnotated": {
+      return new Set([
+        ...freeNames(new Set(boundNames), exp.argType),
+        ...freeNames(new Set([...boundNames, exp.name]), exp.ret),
+      ])
+    }
+
+    case "FnUnfolded": {
+      return freeNames(boundNames, Exps.foldFn(exp.bindings, exp.ret))
+    }
+
+    case "FnUnfoldedWithRetType": {
+      return freeNames(
+        boundNames,
+        Exps.foldFnWithRetType(exp.bindings, exp.retType, exp.ret),
+      )
+    }
+
+    case "Sigma": {
+      return new Set([
+        ...freeNames(boundNames, exp.carType),
+        ...freeNames(new Set([...boundNames, exp.name]), exp.cdrType),
+      ])
+    }
+
+    case "SigmaUnfolded": {
+      return freeNames(boundNames, Exps.foldSigma(exp.bindings, exp.cdrType))
+    }
+
+    case "Cons": {
+      return new Set([
+        ...freeNames(boundNames, exp.car),
+        ...freeNames(boundNames, exp.cdr),
+      ])
+    }
+
+    case "Car": {
+      return new Set([...freeNames(boundNames, exp.target)])
+    }
+
+    case "Cdr": {
+      return new Set([...freeNames(boundNames, exp.target)])
+    }
+
+    case "Quote": {
+      return new Set()
+    }
+
+    case "ClazzNull": {
+      return new Set()
+    }
+
+    case "ClazzCons": {
+      // TODO
+      return new Set()
+    }
+
+    case "ClazzFulfilled": {
+      // TODO
+      return new Set()
+    }
+
+    case "ClazzUnfolded": {
+      // TODO
+      return new Set()
+    }
+
+    case "Objekt": {
+      // TODO
+      return new Set()
+    }
+
+    case "ObjektUnfolded": {
+      // TODO
+      return new Set()
+    }
+
+    case "New": {
+      // TODO
+      return new Set()
+    }
+
+    case "NewUnfolded": {
+      // TODO
+      return new Set()
+    }
+
+    case "NewAp": {
+      // TODO
+      return new Set()
+    }
+
+    case "Dot": {
+      // TODO
+      return new Set()
+    }
+
+    case "SequenceLet": {
+      // TODO
+      return new Set()
+    }
+
+    case "SequenceLetThe": {
+      // TODO
+      return new Set()
+    }
+
+    case "SequenceCheck": {
+      // TODO
+      return new Set()
+    }
+
+    case "SequenceUnfolded": {
       // TODO
       return new Set()
     }
