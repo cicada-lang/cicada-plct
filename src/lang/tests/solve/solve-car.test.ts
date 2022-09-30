fcb6245c0f4de14ddc13a8ac239ad46316284242
Xie Yuheng <xyheme@gmail.com>
2022-09-30 20:58:21 +0800

    add test "solve Car -- nested"

--- src/lang/tests/solve/solve-car.test.ts
+++ src/lang/tests/solve/solve-car.test.ts
@@ -13,3 +13,22 @@ solve (ab: Pair(String, String), a: String) {
 
   expect(output).toMatchInlineSnapshot('"{ ab: cons(\\"a\\", \\"b\\"), a: \\"a\\" }"')
 })
+
+test("solve Car -- nested", async () => {
+  const output = await runCode(`
+
+solve (ab: Pair(String, String), ba: Pair(String, String), a: String, b: String, b2: String) {
+  unify b2 = car(ba)
+  unify ba = cons(b, a)
+  unify a = car(ab)
+  unify b = cdr(ab)
+  unify ab = cons("a", "b")
+
+}
+
+`)
+
+  expect(output).toMatchInlineSnapshot(
+    '"{ ab: cons(\\"a\\", \\"b\\"), ba: cons(\\"b\\", \\"a\\"), a: \\"a\\", b: \\"b\\", b2: b }"',
+  )
+})
