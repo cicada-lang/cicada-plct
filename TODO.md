e839e02fe845714b703e667806d59fd9ea4f4c4f
Xie Yuheng <xyheme@gmail.com>
2022-10-07 03:21:37 +0800

    check-same.test.ts

--- TODO.md
+++ TODO.md
@@ -2,9 +2,6 @@
 
 https://github.com/cicada-lang/cicada/issues/43
 
-Globals.Same
-
-check-same.test.ts
 compute-same.test.ts
 
 Neutrals.Replace
---
+++ src/lang/tests/check/check-same.test.ts
@@ -0,0 +1,34 @@
+import { test } from "vitest"
+import { expectCodeToFail, runCode } from "../utils"
+
+test("check Same", async () => {
+  await runCode(`
+
+check same("abc"): Equal(String, "abc", "abc")
+
+`)
+})
+
+test("check Same -- fail", async () => {
+  await expectCodeToFail(`
+
+check same("abc"): Equal(String, "123", "123")
+
+`)
+})
+
+test("check Same -- given implicit", async () => {
+  await runCode(`
+
+check same(implicit String, "abc"): Equal(String, "abc", "abc")
+
+`)
+})
+
+test("check Same -- given implicit -- fail", async () => {
+  await expectCodeToFail(`
+
+check same(implicit String, "abc"): Equal(String, "123", "123")
+
+`)
+})
