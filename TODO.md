d57b7a559e3f9f1efbdafc13dd7ba6bd4c286db8
Xie Yuheng <xyheme@gmail.com>
2022-10-07 09:37:55 +0800

    add test my_cdr

--- TODO.md
+++ TODO.md
@@ -1,5 +1,3 @@
-add test my_cdr
-
 globals.car
 globals.cdr
 globals.cons
--- src/lang/tests/compute/compute-cdr.test.ts
+++ src/lang/tests/compute/compute-cdr.test.ts
@@ -11,3 +11,22 @@ compute cdr(pair)
 
   expect(output).toMatchInlineSnapshot('"Type: Type"')
 })
+
+test("compute Cdr -- my_cdr", async () => {
+  const output = await runCode(`
+
+function my_cdr(
+  implicit A: Type,
+  implicit B: (x: A) -> Type,
+  pair: exists (x: A) B(x),
+): B(car(pair)) {
+  return cdr(pair)
+}
+
+
+compute my_cdr(cons("a", "b"))
+
+`)
+
+  expect(output).toMatchInlineSnapshot('"\\"b\\": String"')
+})
