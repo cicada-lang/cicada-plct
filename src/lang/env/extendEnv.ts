16a50b72ca62b7cf089ef373b3c1c6d95ac6dc74
Xie Yuheng <xyheme@gmail.com>
2022-11-05 20:39:54 +0800

    up

--- src/lang/env/extendEnv.ts
+++ src/lang/env/extendEnv.ts
@@ -5,7 +5,19 @@ export function extendEnv(env: Env, name: string, value: Value): Env {
   return EnvCons(name, value, substInEnv(env, name, value))
 }
 
+/**
+
+   We need `substInEnv` because implicit application insertion
+   will insert pattern variable
+   and the solution of the pattern variable might be `Neutrals.Var`.
+
+**/
+
 function substInEnv(env: Env, name: string, value: Value): Env {
+  /**
+     TODO I do not know why we need the following line.
+   **/
+
   if (value.kind === "TypedNeutral" && value.neutral.kind === "Var") {
     return env
   }
